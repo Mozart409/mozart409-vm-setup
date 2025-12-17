ed9eac0fb191d06db00442a57fabdfcc7bc116c0
Amadeus Mader <amadeus@mozart409.com>
2025-12-18 00:10:52 +0100

    feat: update setup and add zsh config

--- setup.sh
+++ setup.sh
@@ -35,12 +35,12 @@ install_rust() {
 install_debian() {
   export DEBIAN_FRONTEND=noninteractive
   apt-get update
-  apt-get install -y build-essential wget curl git zip unzip vim net-tools iputils-ping dnsutils netcat-traditional gpg passwd fonts-firacode pkg-config libssl-dev tmux ripgrep sed jq tree btop zsh nodejs lua5.4 luarocks python3 ninja-build gettext cmake
+  apt-get install -y build-essential wget curl git zip unzip vim net-tools iputils-ping dnsutils netcat-traditional gpg passwd fonts-firacode pkg-config libssl-dev tmux ripgrep sed jq tree btop zsh nodejs lua5.4 luarocks python3 ninja-build gettext cmake zsh
 }
 
 install_fedora() {
   dnf update -y
-  dnf install -y wget curl git zip unzip vim net-tools iputils bind-utils nmap-ncat gnupg passwd fira-code-fonts pkg-config openssl-devel tmux ripgrep sed jq tree btop zsh nodejs lua luarocks python3 ninja-build cmake gcc gcc-c++ make
+  dnf install -y wget curl git zip unzip vim net-tools iputils bind-utils nmap-ncat gnupg passwd fira-code-fonts pkg-config openssl-devel tmux ripgrep sed jq tree btop zsh nodejs lua luarocks python3 ninja-build cmake gcc gcc-c++ make zsh
   dnf copr enable agriffis/neovim-nightly
   dnf install -y neovim python3-neovim
 }
@@ -50,8 +50,7 @@ setup_tmux() {
   git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm"
 }
 
-setup_neovim(){
-   git clone https://github.com/a-mader/mozart409-nvim.git ~/.config/nvim
+build_neovim(){
    git clone https://github.com/neovim/neovim /tmp/neovim
    cd /tmp/neovim
    git checkout stable
@@ -61,6 +60,17 @@ setup_neovim(){
    dpkg -i nvim-linux-*.deb
    cd /
    rm -rf /tmp/neovim
+}
+setup_neovim(){
+   git clone https://github.com/a-mader/mozart409-nvim.git ~/.config/nvim
+}
+
+setup_zsh(){
+  wget -O ~/.zshrc https://raw.githubusercontent.com/Mozart409/mozart409-vm-setup/refs/heads/main/zsh/zshrc 
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+  exec zsh 
+
+  oh-my-posh font install iosevka
 }
 
 main() {
@@ -72,13 +82,16 @@ main() {
       install_debian
       install_rust
       setup_tmux
+      build_neovim 
       setup_neovim 
+      setup_zsh 
       ;;
     fedora)
       install_fedora
       install_rust
       setup_tmux 
       setup_neovim 
+      setup_zsh 
       ;;
     *)
       echo "OS not detected." >&2
